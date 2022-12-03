fac034f083c8534a1b226897b59de9ec16402e17
owl <17188713+owlCuri@users.noreply.github.com>
2022-12-03 20:55:50 +0800

    Complete day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,26 +1,43 @@
-pub fn part1(_input: &str) -> ! {
-    todo!()
+pub fn part1(input: &str) -> u32 {
+    input
+        .split("\n\n")
+        .map(|section| {
+            section
+                .split_ascii_whitespace()
+                .map(|num| num.parse::<u32>().unwrap())
+                .sum()
+        })
+        .max()
+        .unwrap_or(0)
 }
 
-pub fn part2(_input: &str) -> ! {
-    todo!()
+pub fn part2(input: &str) -> u32 {
+    let mut calories: Vec<u32> = input
+        .split("\n\n")
+        .map(|section| {
+            section
+                .split_ascii_whitespace()
+                .map(|num| num.parse::<u32>().unwrap())
+                .sum()
+        })
+        .collect();
+    calories.select_nth_unstable_by(2, |a, b| b.cmp(a));
+    calories[0] + calories[1] + calories[2]
 }
 
-#[allow(unreachable_code)]
 #[cfg(test)]
 #[test]
 fn part1_test() {
     assert_eq!(
         part1(&std::fs::read_to_string("input/day01.txt").unwrap()),
-        ()
+        71300
     );
 }
-#[allow(unreachable_code)]
 #[cfg(test)]
 #[test]
 fn part2_test() {
     assert_eq!(
         part2(&std::fs::read_to_string("input/day01.txt").unwrap()),
-        ()
+        209691
     );
 }
