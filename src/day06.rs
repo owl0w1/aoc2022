28cfb465b4c1310d8ffada3df61f824c29e1e096
owl <17188713+owlCuri@users.noreply.github.com>
2022-12-06 13:33:06 +0800

    Complete day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,26 +1,55 @@
-pub fn part1(_input: &str) -> ! {
-    todo!()
+pub fn part1(input: &str) -> u32 {
+    let input = &input.as_bytes()[..input.len() - 1];
+    let mut s = 0;
+    loop {
+        if input[s] == input[s + 1] || input[s] == input[s + 2] || input[s] == input[s + 3] {
+            s += 1;
+            continue;
+        }
+        if input[s + 1] == input[s + 2] || input[s + 1] == input[s + 3] {
+            s += 2;
+            continue;
+        }
+        if input[s + 2] == input[s + 3] {
+            s += 3;
+            continue;
+        }
+        break;
+    }
+    (s + 4) as _
 }
 
-pub fn part2(_input: &str) -> ! {
-    todo!()
+pub fn part2(input: &str) -> u32 {
+    let input = &input.as_bytes()[..input.len() - 1];
+    let mut s = 0;
+    'outer: loop {
+        let mut char_idxs = [usize::MAX; 26];
+        for i in 0..14 {
+            let c: usize = (input[s + i] - b'a') as _;
+            if char_idxs[c] != usize::MAX {
+                s += char_idxs[c] + 1;
+                continue 'outer;
+            }
+            char_idxs[c] = i;
+        }
+        break;
+    }
+    (s + 14) as _
 }
 
-#[allow(unreachable_code)]
 #[cfg(test)]
 #[test]
 fn part1_test() {
     assert_eq!(
         part1(&std::fs::read_to_string("input/day06.txt").unwrap()),
-        ()
+        1707
     );
 }
-#[allow(unreachable_code)]
 #[cfg(test)]
 #[test]
 fn part2_test() {
     assert_eq!(
         part2(&std::fs::read_to_string("input/day06.txt").unwrap()),
-        ()
+        3697
     );
 }
